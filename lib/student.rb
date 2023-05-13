5c14cfddceeeafa18c7f9a55d38480890ac98d13
John <johnfarris217@gmail.com>
2023-05-12 22:38:26 -0500

    commit tree dc6ef676ca35db9c6b60f72d14565f5127ccda34

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,49 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :id, :name, :grade
 
+  def initialize(name, grade)
+    @id = nil
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)")
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      self
+    end
+  end
+
+  def self.create(name, grade)
+    Student.new(name, grade).save
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new(row[1], row[2])
+    new_student.id = row[0]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM students WHERE name = ? LIMIT 1", name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    DB[:conn].execute("UPDATE students SET name = ?, grade = ? WHERE id = ?", self.name, self.grade, self.id)
+  end
 end
